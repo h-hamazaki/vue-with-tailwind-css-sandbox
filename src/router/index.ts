913ea6892375660cad9665cb589687bb93251f59
h-hamazaki <homare.hamazaki@myu-on.co.jp>
2021-07-25 11:26:32 +0900

    setup Tailwind CSS

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,25 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
+import Home from "../views/Home.vue"
+import Sample from "@/views/screens/Sample.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/sample",
+    name: "Sample",
+    component: Sample,
   }
 ]
 
